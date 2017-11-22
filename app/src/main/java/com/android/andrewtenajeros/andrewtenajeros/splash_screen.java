c83efe25889b4552a52ede133da5f1a99127ecd4
Andrew Tenajeros <jules.tenajeros@gmail.com>
2017-11-22 20:07:06 +0800

    Initial commit

---
+++ app/src/main/java/com/android/andrewtenajeros/andrewtenajeros/splash_screen.java
@@ -0,0 +1,16 @@
+package com.android.andrewtenajeros.andrewtenajeros;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+
+}
