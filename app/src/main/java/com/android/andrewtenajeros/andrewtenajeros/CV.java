9113b0324a2f7d073238ec2206170adfe4a83fda
Andrew Tenajeros <jules.tenajeros@gmail.com>
2017-11-22 22:01:20 +0800

    2

--- app/src/main/java/com/android/andrewtenajeros/andrewtenajeros/CV.java
+++ app/src/main/java/com/android/andrewtenajeros/andrewtenajeros/CV.java
@@ -11,19 +11,18 @@ public class CV extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash_screen);
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 setContentView(R.layout.activity_cv);
             }
-        },1000);
+        },1500);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setTitle("Andrew Tenajeros");
         setSupportActionBar(toolbar);
 
-
-    }
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
